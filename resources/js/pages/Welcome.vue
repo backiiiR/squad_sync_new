2bdfc1eeb8ff5b0588f7b3200dbaf78c1c19c51a
backiiiR <robin.backnecht@gmail.com>
2025-03-06 05:03:47 +0100

    change button text

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -42,7 +42,7 @@ import { Head, Link } from '@inertiajs/vue3';
                                 :href="route('login')"
                                 class="inline-block rounded-sm border border-black bg-[#1b1b18] px-5 py-1.5 text-sm leading-normal text-white hover:border-black hover:bg-black dark:border-[#eeeeec] dark:bg-[#eeeeec] dark:text-[#1C1C1A] dark:hover:border-white dark:hover:bg-white"
                             >
-                                Deploy now
+                                Go to application
                             </Link>
                         </li>
                     </ul>
